213363c7e741ae3992c27952f90264a655a9a7f2
benjy8001 <benjy80@gmail.com>
2022-04-04 00:05:14 +0200

    Edit form and validation via Livewire

--- app/Http/Livewire/Channel/EditChannel.php
+++ app/Http/Livewire/Channel/EditChannel.php
@@ -3,13 +3,27 @@
 namespace App\Http\Livewire\Channel;
 
 use App\Models\Channel;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class EditChannel extends Component
 {
-    /** @var string */
+    /** @var Channel */
     public $channel;
+
+    /**
+     * @return string[]
+     */
+    protected function rules(): array
+    {
+        return[
+            'channel.name' => 'required|max:255|unique:channels,name,' . $this->channel->id,
+            'channel.slug' => 'required|max:255|unique:channels,slug,' . $this->channel->id,
+            'channel.description' => 'nullable|max:1024',
+        ];
+    }
 
     /**
      * @param Channel $channel
@@ -19,8 +33,28 @@ class EditChannel extends Component
         $this->channel = $channel;
     }
 
+    /**
+     * @return View
+     */
     public function render(): View
     {
         return view('livewire.channel.edit-channel');
+    }
+
+    /**
+     * @return Redirector
+     */
+    public function update(): Redirector
+    {
+        $this->validate();
+        $this->channel->update([
+            'name' => $this->channel->name,
+            'slug' => $this->channel->slug,
+            'description' => $this->channel->description,
+        ]);
+
+        session()->flash('message', 'Channel updated');
+
+        return redirect()->route('channel.edit', ['channel' => $this->channel->slug]);
     }
 }
--- resources/views/livewire/channel/edit-channel.blade.php
+++ resources/views/livewire/channel/edit-channel.blade.php
@@ -1,3 +1,49 @@
 <div>
     {{ $channel->name }}
+
+    <form wire:submit.prevent="update">
+        <div class="row mb-3">
+            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" wire:model="channel.name">
+                @error('channel.name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="slug" class="col-md-4 col-form-label text-md-end">{{ __('Slug') }}</label>
+            <div class="col-md-6">
+                <input type="text" class="form-control" wire:model="channel.slug">
+                @error('channel.slug')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
+            <div class="col-md-6">
+                <textarea class="form-control" cols="30" rows="4" wire:model="channel.description"></textarea>
+                @error('channel.description')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <button type="submit" class="btn btn-primary">{{ __('Update') }}</button>
+        </div>
+
+        @if (session()->has('message'))
+            <div class="alert alert-success" role="alert">
+                {{ session('message') }}
+            </div>
+        @endif
+    </form>
 </div>
