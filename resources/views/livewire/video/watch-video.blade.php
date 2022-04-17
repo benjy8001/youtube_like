ef6db1a66f90b91e00656fe9141427e42ec08ac7
benjy8001 <benjy80@gmail.com>
2022-04-17 23:16:55 +0200

    Fix Video Player timeline  and Add Poster image

--- resources/views/livewire/video/watch-video.blade.php
+++ resources/views/livewire/video/watch-video.blade.php
@@ -5,8 +5,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 p-0">
-                <div class="video-container">
-                    <video controls preload="auto" id="yt-video" class="video-js vjs-fill vjs-styles=defaults vjs-big-play-centered" data-setup="{}" wire:ignore>
+                <div class="video-container" wire:ignore>
+                    <video controls preload="auto" id="yt-video"
+                           class="video-js vjs-fill vjs-styles=defaults vjs-big-play-centered"
+                           data-setup="{}"
+                           poster="{{ secure_asset('videos/' . $video->uid . '/' . $video->thumbnail_image) }}">
                         <source src="{{ secure_asset('videos/' . $video->uid . '/' . $video->processed_file) }}" type="application/x-mpegURL" />
                         <p class="vjs-no-js">
                             To view this video please enable JavaScript, and consider upgrading to a
